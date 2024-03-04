cbd2226ae75a7bdfd495430137dbed6c0412b307
Mth830 <mth830@gmail.com#>
2024-03-04 06:38:53 -0500

    first commit

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -14,19 +14,18 @@ class DynamicArray {
   }
 
   unshift(val) {
-    //if the length <capacity push shift everything anove move to the front
-
 
     if (this.length < this.capacity) {
       
-      //if the length <capacity push shift everything anove move to the right
+      //if the length < capacity  shift everything to the right and add data at index 0
       for (let i = this.length; i >= 1; i--) {
         this.data[i] = this.data[i - 1];
       }
+      this.length++;
       this.data[0] = val;
     
       //if length is equal to the capacity make a new Array and copy contents 
-      //shifted    
+      //shifted  then add data to index 0 and add data to index 0 and update capacity   
     }else if (this.length === this.capacity) {
 
       let arr = new Array(this.capacity + 1);
